f24a4efde8880a2cf766cb6f02291afda85d716a
unknown <SENA@MEDAPRCGEFSP571.FormacionSena.RED>
2024-05-10 16:02:56 -0500

    feat: Ejercicios de variables

--- 01-sintaxis-variables/index.php
+++ 01-sintaxis-variables/index.php
@@ -1,3 +1,23 @@
 <?php
-echo 'Ejecutando sintaxis variable...';
+$saludo = "Hola";
+$nombre1 = "pedro";
+$nombre2 = "Ana";
+
+echo "$saludo $nombre1 y $nombre2 ¿Como estan?";
+
+echo "<br> <ul> Pagina Creada Por: <br> -$nombre1 <br> -$nombre2"
+?>
+
+<br>
+<br>
+
+<?php
+$mi_numero = "6";
+$respuesta = $mi_numero;
+$respuesta += "2";
+$respuesta *= "2";
+$respuesta -= "2";
+$respuesta /= "2";
+$respuesta -= $mi_numero;
+echo "$respuesta";
 ?>
