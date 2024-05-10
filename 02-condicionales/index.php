829bdb74d97cb3953c181a49ca8be0191e6aef3e
unknown <SENA@MEDAPRCGEFSP571.FormacionSena.RED>
2024-05-10 17:32:08 -0500

    feat: Ejercicios de condicionales

--- 02-condicionales/index.php
+++ 02-condicionales/index.php
@@ -1,3 +1,80 @@
 <?php
-echo 'Ejecutando condicionales...';
+
+$Cliente = "17";
+if($Cliente >="12"){
+echo "Caja Normal";
+}
+else if($Cliente <="12"){
+  echo "Caja Rapida";  
+}
+
+echo "<br>";
+echo "<br>";
+
+
+$Cliente = "7";
+if($Cliente >="12"){
+echo "Caja Normal";
+}
+else if($Cliente <="12"){
+  echo "Caja Rapida";  
+}
+
+echo "<br>";
+echo "<br>";
+
+
+
+$Nombre = "Mario";
+
+if($Nombre != "Mario"){
+echo "Puedes ingresar al VIP";   
+ }
+    
+ else{
+echo "no puedes ingresar al VIP";
+
+ }
+
+ echo "<br>";
+$Edad = 18;
+
+if($Edad < 18){
+echo "Puedes ingresar a la discoteca";   
+}
+
+else{
+echo "no puede ingresar a la discoteca";    
+}
+
+
+
+echo "<br>";
+echo "<br>";
+
+
+
+
+$Nombre = "Mario";
+
+if($Nombre == "Mario"){
+echo "Puedes ingresar al VIP";   
+ }
+    
+ else{
+echo "no puedes ingresar al VIP";
+
+ }
+
+ echo "<br>";
+$Edad = 18;
+
+if($Edad >= 18){
+echo "Puedes ingresar a la discoteca";   
+}
+
+else{
+echo "no puede ingresar a la discoteca";    
+}
 ?>
+
