42a97b03cc602c0e17473accfffb8b6280b849c8
unknown <SENA@MEDAPRCGEFSP571.FormacionSena.RED>
2024-05-14 16:07:51 -0500

    loops-php

---
+++ 03-php loops/index.php
@@ -0,0 +1,65 @@
+<?php
+
+$i = 2;
+while ($i <= 30) {
+  //$i*=2;
+  echo "2 X $i = ".($i*2)."<br>";
+  $i++;
+}
+/* 
+echo "<br>";
+
+$i = 2;
+while ($i < 3) {
+  $i*=3;
+  echo "2 X 3 = $i";
+}
+
+echo "<br>";
+$i = 2;
+while ($i < 4) {
+  $i*=4;
+  echo "2 X 4 = $i";
+}
+
+echo "<br>";
+$i = 2;
+while ($i < 5) {
+  $i*=5;
+  echo "2 X 5 = $i";
+}
+echo "<br>";
+$i = 2;
+while ($i < 6) {
+  $i*=6;
+  echo "2 X 6 = $i";
+}
+
+echo "<br>";
+$i = 2;
+while ($i < 7) {
+  $i*=7;
+  echo "2 X 7 = $i";
+}
+
+echo "<br>";
+$i = 2;
+while ($i < 8) {
+  $i*=8;
+  echo "2 X 8 = $i";
+}
+
+echo "<br>";
+$i = 2;
+while ($i < 9) {
+  $i*=9;
+  echo "2 X 9 = $i";
+}
+
+echo "<br>";
+$i = 2;
+while ($i < 10) {
+  $i*=10;
+  echo "2 X 10 = $i";
+} */
+?>
