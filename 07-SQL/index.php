f28b9739594d753ff4748c5b744dad8b3eee63b3
Julian Jaramillo <julianjara400@gmail.com>
2024-05-21 16:42:44 -0500

    "feat" sql

---
+++ 07-SQL/index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+//Ejemplos:
+
+//1. SELECT 
+//SELECT nombre, Edad FROM profesores;
+//Nos ayuda a seleccionar y mostrar solo lo que le pedimos que nos muestre
+
+//2.Select-Distintic
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
